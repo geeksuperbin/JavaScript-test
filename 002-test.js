37c8112a86379aafa5c8afd8107503dc6321efcb
geeksuperbin <liubindashen@gmail.com>
2022-02-22 13:18:37 +0800

    js test

---
+++ 002-test.js
@@ -0,0 +1,46 @@
+var a = 1 + 3;var b = 'abc';
+
+console.log(a);
+1+3;
+;;;
+
+// js 的变量名区分大小写
+var a = 1;
+var A = 2;
+
+console.log(a);
+console.log(A);
+
+// 变量的声明和赋值
+// 是分开的两个步骤
+var c;
+c = 4;
+console.log(c);
+
+
+var d;
+console.log(d);
+
+
+var e = 1;
+e = 5;
+console.log(e);
+
+// f
+
+
+var 你好, 中国;
+
+你好 = 6;
+中国  = 9;
+
+console.log(你好);
+console.log(中国);
+
+
+
+
+
+
+
+
